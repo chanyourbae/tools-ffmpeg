dfc5c44975fb94739581af2cd0888496b02b33b6
chanyourbae <ichankulon@gmail.com>
2026-06-16 04:06:15 +0700

    Add files via upload

---
+++ ijkl.js
@@ -0,0 +1,318 @@
+function decodeStr1(arg_1, arg_2, arg_3, arg_4) {
+  return decodeString(arg_1 - 0x2a9, arg_3);
+}
+function decodeString(arg_5, arg_6) {
+  arg_5 = arg_5 - (0x1b * 0x11a + 0x5b * 0x59 + 0x25 * -0x1a1);
+  const result_3 = stringArray();
+  let val_1 = result_3[arg_5];
+  if (decodeString["KUyOql"] === undefined) {
+    var fn_1 = function (arg_7) {
+      const str_1 =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=";
+      let str_2 = "",
+        str_3 = "";
+      for (
+        let i_1 = 0x1 * -0x26f + -0x2 * -0xeb9 + -0x1b03,
+          val_5,
+          val_6,
+          i_2 = -0x1 * -0x20f6 + -0xf49 + 0x5 * -0x389;
+        (val_6 = arg_7["charAt"](i_2++));
+        ~val_6 &&
+        ((val_5 =
+          i_1 % (-0x1 * 0x143d + 0x12ff + 0x142)
+            ? val_5 * (0x12af + -0x1ed9 + 0xc6a) + val_6
+            : val_6),
+        i_1++ % (0x673 * -0x1 + -0x90c + 0xf83))
+          ? (str_2 += String["fromCharCode"](
+              (-0x80 * 0x3b + 0x10c2 + -0xdbd * -0x1) &
+                (val_5 >>
+                  ((-(0x412 + 0x2492 * -0x1 + 0x72 * 0x49) * i_1) &
+                    (0x7f * -0x11 + 0x1afe + 0x3b5 * -0x5))),
+            ))
+          : 0x5 * 0x72b + 0x23bb * -0x1 + -0x2 * 0xe
+      ) {
+        val_6 = str_1["indexOf"](val_6);
+      }
+      for (
+        let i_3 = 0x1 * -0xdab + 0x3dc + 0x9cf, len_1 = str_2["length"];
+        i_3 < len_1;
+        i_3++
+      ) {
+        str_3 +=
+          "%" +
+          ("00" +
+            str_2["charCodeAt"](i_3)["toString"](
+              0x5 * -0xd + 0xbe8 + -0x17 * 0x81,
+            ))["slice"](-(-0x22e4 + 0x1243 * -0x2 + 0xa34 * 0x7));
+      }
+      return decodeURIComponent(str_3);
+    };
+    ((decodeString["HBSkiK"] = fn_1),
+      (decodeString["VKaOSf"] = {}),
+      (decodeString["KUyOql"] = !![]));
+  }
+  const val_2 = result_3[-0x2e6 + 0x377 * -0x1 + 0x65d],
+    val_3 = arg_5 + val_2,
+    val_4 = decodeString["VKaOSf"][val_3];
+  return (
+    !val_4
+      ? ((val_1 = decodeString["HBSkiK"](val_1)),
+        (decodeString["VKaOSf"][val_3] = val_1))
+      : (val_1 = val_4),
+    val_1
+  );
+}
+(function (arg_8, arg_9) {
+  function decodeStr2(arg_10, arg_11, arg_12, arg_13) {
+    return decodeString(arg_13 - -0xba, arg_11);
+  }
+  function decodeStr3(arg_14, arg_15, arg_16, arg_17) {
+    return decodeString(arg_16 - 0x2f3, arg_15);
+  }
+  const result_4 = arg_8();
+  while (!![]) {
+    try {
+      const val_7 =
+        (parseInt(decodeStr3(0x437, 0x441, 0x447, 0x435)) /
+          (0xf2 * -0x11 + 0xf6f + 0x2 * 0x52)) *
+          (-parseInt(decodeStr3(0x418, 0x433, 0x428, 0x43a)) /
+            (0x16e6 + -0xd * 0x221 + 0x4c9)) +
+        parseInt(decodeStr3(0x44b, 0x42f, 0x42e, 0x423)) /
+          (-0x53d + 0x33 * -0xc1 + 0x3 * 0xe91) +
+        parseInt(decodeStr2(0x80, 0xb1, 0x88, 0xa1)) /
+          (0x503 + 0x249b + -0x299a) +
+        -parseInt(decodeStr3(0x42d, 0x414, 0x42c, 0x40f)) /
+          (-0x1 * -0xa4c + -0x1f47 + 0x1500) +
+        -parseInt(decodeStr3(0x444, 0x42a, 0x435, 0x424)) /
+          (-0x1ecc + 0x1 * -0x25fe + 0x44d0) +
+        -parseInt(decodeStr2(0x65, 0x6d, 0x8f, 0x84)) /
+          (0x2d1 + -0x1d68 + 0x1a9e) +
+        (parseInt(decodeStr3(0x40d, 0x3f3, 0x411, 0x3f3)) /
+          (-0x12f4 + 0x69f + 0x41f * 0x3)) *
+          (parseInt(decodeStr3(0x44f, 0x43a, 0x452, 0x474)) /
+            (-0x235c * 0x1 + 0x1 * -0x6ae + 0x2a13));
+      if (val_7 === arg_9) break;
+      else result_4["push"](result_4["shift"]());
+    } catch (error_1) {
+      result_4["push"](result_4["shift"]());
+    }
+  }
+})(stringArray, 0x48d * 0x1e7 + -0x64f3 + -0x25f2b);
+function decodeStr4(arg_18, arg_19, arg_20, arg_21) {
+  return decodeString(arg_21 - -390, arg_19);
+}
+const result_1 = (function () {
+    let val_8 = !![];
+    return function (arg_22, arg_23) {
+      const val_9 = val_8
+        ? function () {
+            function decodeStr5(arg_24, arg_25, arg_26, arg_27) {
+              return decodeString(arg_25 - 0x134, arg_27);
+            }
+            if (arg_23) {
+              const result_5 = arg_23["apply"](arg_22, arguments);
+              return ((arg_23 = null), result_5);
+            }
+          }
+        : function () {};
+      return ((val_8 = ![]), val_9);
+    };
+  })(),
+  result_2 = result_1(this, function () {
+    const obj_1 = {};
+    ((obj_1["bsCYH"] = function (arg_28, arg_29) {
+      return arg_28 + arg_29;
+    }),
+      (obj_1["nWSrG"] = "return\x20(fu" + "nction() "),
+      (obj_1["pQlZd"] = "{}.constru" + 'ctor("retu' + 'rn this")(' + "\x20)"),
+      (obj_1["lofjr"] = "log"),
+      (obj_1["VfoNC"] = "warn"));
+    function decodeStr6(arg_30, arg_31, arg_32, arg_33) {
+      return decodeString(arg_30 - 0x1f6, arg_31);
+    }
+    ((obj_1["LdkUw"] = "error"), (obj_1["DAonq"] = "table"));
+    function decodeStr7(arg_34, arg_35, arg_36, arg_37) {
+      return decodeString(arg_35 - 0x1f3, arg_34);
+    }
+    obj_1["yswWa"] = function (arg_38, arg_39) {
+      return arg_38 < arg_39;
+    };
+    const val_10 = obj_1,
+      fn_2 = function () {
+        function decodeStr8(arg_40, arg_41, arg_42, arg_43) {
+          return decodeStr7(
+            arg_43,
+            arg_41 - 0x9d,
+            arg_42 - 0x19,
+            arg_43 - 0x1a6,
+          );
+        }
+        let val_12;
+        try {
+          val_12 = Function(
+            val_10["bsCYH"](val_10["nWSrG"] + val_10["pQlZd"], ");"),
+          )();
+        } catch (error_2) {
+          val_12 = window;
+        }
+        function decodeStr9(arg_44, arg_45, arg_46, arg_47) {
+          return decodeStr7(
+            arg_44,
+            arg_45 - -530,
+            arg_46 - 0x1b1,
+            arg_47 - 0x121,
+          );
+        }
+        return val_12;
+      },
+      result_6 = fn_2(),
+      val_11 = (result_6["console"] = result_6["console"] || {}),
+      arr_1 = [
+        val_10["lofjr"],
+        val_10["VfoNC"],
+        "info",
+        val_10["LdkUw"],
+        "exception",
+        val_10["DAonq"],
+        "trace",
+      ];
+    for (let i_4 = 0; val_10["yswWa"](i_4, arr_1["length"]); i_4++) {
+      const result_7 =
+          result_1["constructo" + "r"]["prototype"]["bind"](result_1),
+        val_13 = arr_1[i_4],
+        val_14 = val_11[val_13] || result_7;
+      ((result_7["__proto__"] = result_1["bind"](result_1)),
+        (result_7["toString"] = val_14["toString"]["bind"](val_14)),
+        (val_11[val_13] = result_7));
+    }
+  });
+(result_2(),
+  (window["processYzM" + "ethod"] = async function () {
+    const obj_2 = {};
+    ((obj_2["yFqyV"] = "input[name" + '="yzOpt"]:' + "checked"),
+      (obj_2["csKYs"] = "writeFile"),
+      (obj_2["hpRCh"] = function (arg_48, arg_49) {
+        return arg_48 === arg_49;
+      }),
+      (obj_2["TfwUR"] = "fast"),
+      (obj_2["PfDIn"] = "in.mp4"),
+      (obj_2["PrrOz"] = "unlink"),
+      (obj_2["AZGMJ"] = "out.ts"));
+    function decodeStr10(arg_50, arg_51, arg_52, arg_53) {
+      return decodeStr4(arg_50 - 0xfb, arg_53, arg_52 - 0x1a2, arg_50 - -512);
+    }
+    ((obj_2["UKVKe"] = "-b:v"),
+      (obj_2["XEaJo"] = "out.mp4"),
+      (obj_2["bAySr"] = "readFile"));
+    const val_15 = obj_2;
+    let val_16 = document["querySelec" + "tor"](val_15["yFqyV"])["value"];
+    function decodeStr11(arg_54, arg_55, arg_56, arg_57) {
+      return decodeStr4(arg_54 - 0x1be, arg_56, arg_56 - 0x170, arg_54 - -472);
+    }
+    let val_17 =
+      "YZ_" +
+      window["selectedFi" + "le"]["name"]["replace"](/\.[^/.]+$/, "") +
+      ".mp4";
+    window["ffmpeg"]["FS"](
+      val_15["csKYs"],
+      "in.mp4",
+      await FFmpeg["fetchFile"](window["selectedFi" + "le"]),
+    );
+    if (val_15["hpRCh"](val_16, val_15["TfwUR"])) {
+      await window["ffmpeg"]["run"](
+        "-i",
+        val_15["PfDIn"],
+        "-c",
+        "copy",
+        "out.ts",
+      );
+      let result_8 = window["ffmpeg"]["FS"]("readFile", "out.ts");
+      (window["downloadBl" + "ob"](result_8["buffer"], val_17),
+        window["ffmpeg"]["FS"](val_15["PrrOz"], val_15["AZGMJ"]));
+    } else {
+      let val_18 = window["currentBit" + "rateKbps"]
+        ? [val_15["UKVKe"], window["currentBit" + "rateKbps"] + "k"]
+        : [];
+      await window["ffmpeg"]["run"]("-i", "in.mp4", ...val_18, val_15["XEaJo"]);
+      let result_9 = window["ffmpeg"]["FS"](val_15["bAySr"], val_15["XEaJo"]);
+      (window["downloadBl" + "ob"](result_9["buffer"], val_17),
+        window["ffmpeg"]["FS"]("unlink", val_15["XEaJo"]));
+    }
+    window["ffmpeg"]["FS"](val_15["PrrOz"], val_15["PfDIn"]);
+  }));
+function stringArray() {
+  const arr_2 = [
+    "mZq0mtqWyLPOq3nI",
+    "EuzXEvy",
+    "vMzVtKm",
+    "Aw5WDxrBBMfTzq",
+    "mJK4otG1mejIsKHuqG",
+    "y29UC29Szq",
+    "nZy2nZu4zgTgufjm",
+    "Exn3v2e",
+    "y3nlwxm",
+    "mJm1nZC2oeD4r0nJBq",
+    "E30Uy29UC3rYDq",
+    "qvPhtuO",
+    "CMvHzezPBgu",
+    "mZe2nJaWohntrKDjvW",
+    "Ahbsq2G",
+    "ugzesw4",
+    "yKf5u3i",
+    "Aw4UBxa0",
+    "vuTws2u",
+    "ChjVy2vZC1L6tq",
+    "C2vSzwn0zwrgAq",
+    "BgvUz3rO",
+    "CMvWBgfJzq",
+    "Bg9MANi",
+    "DMfSDwu",
+    "Aw5MBW",
+    "Bg9N",
+    "D3jPDgvgAwXL",
+    "zxHJzxb0Aw9U",
+    "x19WCM90B19F",
+    "zMzTCgvN",
+    "muDjELvPua",
+    "yNndwuG",
+    "refVBNe",
+    "CxvLCNLtzwXLyW",
+    "DhjHy2u",
+    "psj5EK9WDcjDoG",
+    "uhjYt3O",
+    "mJqXnZa1nLLsDw9msq",
+    "zxrOB2q",
+    "DgfIBgu",
+    "CM4GDgHPCYiPka",
+    "ndiZELreAgzr",
+    "CNvU",
+    "BMn0Aw9UkcKG",
+    "mtK3ntC2DuXnteXQ",
+    "y3vYCMvUDejPDa",
+    "vgz3vvi",
+    "Dw5SAw5R",
+    "zxjYB3i",
+    "BMfTzq",
+    "CMf0zuTIChm",
+    "yNvMzMvY",
+    "tgrRvxC",
+    "wvPF",
+    "y3rVCIGICMv0Dq",
+    "yMLUza",
+    "lwi6DG",
+    "yxbWBhK",
+    "B3v0lM1Wna",
+    "B3v0lNrZ",
+    "CffSwMq",
+    "Dg9tDhjPBMC",
+    "y2HLy2TLza",
+    "Dg9Y",
+    "zg93BMXVywrcBa",
+    "wevHsM8",
+    "y29WEq",
+  ];
+  stringArray = function () {
+    return arr_2;
+  };
+  return stringArray();
+}
